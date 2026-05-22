7985fd212438de9a1aeb8137a363deb5919157b9
chacha-mu <1804398@fuku-c.ed.jp>
2026-05-22 12:24:13 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,190 @@
+const rouletteCats = [
+
+  {
+    name: "スコティッシュフォールド",
+    image: "images/スコ.jpeg",
+    text: "折れた耳とまんまるな顔が魅力の人気猫。おっとりした性格で、人に甘えるのが大好きな猫です。"
+  },
+
+  {
+    name: "マンチカン",
+    image: "images/マンチカン.jpeg",
+    text: "短い足で一生懸命走る姿がとてもかわいい遊ぶことが大好きで元気いっぱいな猫です。"
+  },
+
+  {
+    name: "ラグドール",
+    image: "images/ラグドール.jpeg",
+    text: "ふわふわの毛並みと青い目が美しい穏やかで抱っこされるのが好きな癒し系の猫です。"
+  },
+
+  {
+    name: "アメリカンショートヘア",
+    image: "images/アメショ.jpeg",
+    text: "丈夫で飼いやすく初心者にも人気で元気で遊び好きな猫です。"
+  },
+
+  {
+    name: "ロシアンブルー",
+    image: "images/ロシアンブルー.jpeg",
+    text: "美しいグレーの毛並みとエメラルドの瞳が特徴の静かで上品な猫です。"
+  },
+
+  {
+    name: "ノルウェージャンフォレストキャット",
+    image: "images/ノルウェー.jpeg",
+    text: "北欧生まれで長くふわふわな毛と優しい性格が人気の大型猫です。"
+  },
+
+  {
+    name: "ベンガル",
+    image: "images/ベンガル.jpeg",
+    text: "ヒョウのような模様が特徴で運動神経が良くとても活発な猫です。"
+  },
+
+  {
+    name: "ペルシャ",
+    image: "images/ペルシャ.jpeg",
+    text: "長い毛並みと高級感ある見た目が魅力のおっとりした優雅な猫です。"
+  },
+
+  {
+    name: "シャム",
+    image: "images/シャム.jpeg",
+    text: "スリムな体が特徴で人懐っこくおしゃべり好きな猫です。"
+  },
+
+  {
+    name: "メインクーン",
+    image: "images/メイン.jpeg",
+    text: "大型で迫力がありながらとても優しく穏やかな人気の猫です。"
+  },
+
+  {
+    name: "エキゾチックショートヘア",
+    image: "images/エキゾ.jpeg",
+    text: "ぺちゃんこな顔と丸い体がかわいいのんびりした癒し系の猫です。"
+  },
+
+  {
+    name: "サイベリアン",
+    image: "images/サイベリアン.jpeg",
+    text: "モフモフした長い毛が特徴で優しく甘えん坊な猫です。"
+  },
+
+  {
+    name: "日本猫",
+    image: "images/ダウンロード.jpeg",
+    text: "昔から日本で親しまれてきた丈夫で賢く人になじみやすい猫です。。"
+  }
+
+];
+
+const button =
+  document.getElementById("rouletteButton");
+
+button.addEventListener("click", startRoulette);
+
+let spinning = false;
+
+function startRoulette() {
+document.getElementById("catImage").style.display =
+  "block";
+
+document.querySelector(".start-message").style.display =
+  "none";
+  if (spinning) {
+    return;
+  }
+
+  spinning = true;
+
+  button.textContent =
+    "でゅらららららら...";
+
+  button.style.background =
+    "#ff4444";
+
+  let speed = 40;
+
+  let count = 0;
+
+  shuffle(speed, count);
+
+}
+
+function shuffle(speed, count) {
+
+  const random =
+    rouletteCats[
+      Math.floor(
+        Math.random() * rouletteCats.length
+      )
+    ];
+
+  document.getElementById("catImage").src =
+    random.image;
+
+  document.getElementById("catName").textContent =
+    random.name;
+
+  document.getElementById("catText").textContent =
+    random.text;
+
+  count++;
+
+  if (count < 45) {
+
+    if (count > 15) {
+
+      speed += 8;
+
+    }
+
+    if (count > 30) {
+
+      speed += 15;
+
+    }
+
+    setTimeout(function() {
+
+      shuffle(speed, count);
+
+    }, speed);
+
+  } else {
+
+    document.querySelector(".roulette-card")
+      .style.transform =
+      "scale(1.12)";
+
+    document.querySelector(".roulette-card")
+      .style.transition =
+      "0.25s";
+
+    button.textContent =
+      "ダン！！";
+　　document
+  .getElementById("finishSound")
+  .play();
+    button.style.background =
+      "#ff9900";
+
+    setTimeout(function() {
+
+      document.querySelector(".roulette-card")
+        .style.transform =
+        "scale(1)";
+
+      button.textContent =
+        "もう一回まわす！";
+
+      spinning = false;
+
+    }, 800);
+
+  }
+}
+document.getElementById("catImage").style.display =
+  "none";
